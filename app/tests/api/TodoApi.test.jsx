95f21e6b12f0243ccd9eb82f729df4e777d18ccd
BlueAnimal81 <blueanimal@me.com>
2017-03-06 08:02:31 +0000

    Wire up search

--- app/tests/api/TodoApi.test.jsx
+++ app/tests/api/TodoApi.test.jsx
@@ -52,4 +52,47 @@ describe('TodoApi', () => {
       expect(actualTodos).toEqual(todos);
     });
   });
+
+  describe('filterTodos', () => {
+    var todos = [{
+      id: 1,
+      text: 'Some Text',
+      completed: true
+    },{
+      id: 2,
+      text: 'Other Text',
+      completed: false
+    },{
+      id: 3,
+      text: 'Some Text',
+      completed: true
+    }];
+
+    it('should return all items if showCompleted is true', () => {
+      var filteredTodos = TodoApi.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return non-completed items if showCompleted is false', () => {
+      var filteredTodos = TodoApi.filterTodos(todos, false, '');
+      expect(filteredTodos.length).toBe(1);
+    })
+
+    it('should sort by completed status', () => {
+      var filteredTodos = TodoApi.filterTodos(todos, true, '');
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should filter items by searchText', () => {
+      var filteredTodos = TodoApi.filterTodos(todos, true, 'some');
+      expect(filteredTodos.length).toBe(2);
+    })
+
+    it('should return all items if searchText is empty', () => {
+      var filteredTodos = TodoApi.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    })
+
+
+  });
 });
